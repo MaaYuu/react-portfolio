a1ba0a6687e28cc8d533157007578b3358d88494
Mahmut Yüksel <mhmt.yksl93@gmail.com>
2022-07-10 10:02:39 +0300

    logo commit

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,19 +1,37 @@
-import './index.scss';
+import './index.scss'
 import LogoTitle from '../../assets/images/logo-s.png'
-import {Link} from 'react-router-dom'
-import { useState, useEffect } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
-
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ['a', 'h', 'm', 'u', 't']
-  const jobArray = ['f', 'r', 'o', 'n', 't', 'e', 'n', 'd', '',
-                    'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+  const jobArray = [
+    'f',
+    'r',
+    'o',
+    'n',
+    't',
+    'e',
+    'n',
+    'd',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
 
   useEffect(() => {
-    return setTimeout(() => {
-      setLetterClass('text-animate-hover')
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
     }, 4000)
   }, [])
 
@@ -26,22 +44,25 @@ const Home = () => {
           <br />
           <span className={`${letterClass} _13`}>I</span>
           <span className={`${letterClass} _14`}>'m</span>
-        <img src={LogoTitle} alt="developer" />
-        <AnimatedLetters
-          letterClass={letterClass}
-          strArray={nameArray}
-          idx={15}
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
           />
-        <br />
-        <AnimatedLetters
-          letterClass={letterClass}
-          strArray={jobArray}
-          idx={20}
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={20}
           />
         </h1>
         <h2> Junior Web Developer </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
+      <Logo />
     </div>
   )
 }
